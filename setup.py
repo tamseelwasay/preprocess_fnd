6059091317c956cab00129b2fd145b70eae49549
tamseelwasay <126204104+tamseelwasay@users.noreply.github.com>
2023-02-23 21:13:13 +0000

    first commit
    first

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as file:
+	long_description = file.read()
+
+
+setuptools.setup(
+	name = 'preprocess_fnd', #this should be unique
+	include_package_data=True,
+	version = '0.1.3',
+	author = 'Laxmi Kant',
+	author_email = 'udemy@kgptalkie.com',
+	description = 'This is preprocessing package',
+	long_description = long_description,
+	long_description_content_type = 'text/markdown',
+	packages = setuptools.find_packages(),
+	classifiers = [
+	'Programming Language :: Python :: 3',
+	'License :: OSI Aproved :: MIT License',
+	"Operating System :: OS Independent"],
+	python_requires = '>=3.5'
+	)
